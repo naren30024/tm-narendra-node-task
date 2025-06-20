b17e4370555634f56893e4bab656d734ba7b1317
naren30024 <naren30024@gmail.com>
2025-06-20 23:31:40 +0530

    add route for single user registration

--- src/routes/auser.route.js
+++ src/routes/auser.route.js
@@ -1,5 +1,7 @@
 const express = require("express");
-const {  get_city_aadhar_user, user_registration, get_aadhar_count_s, get_aadhar_user_sc } = require("../controllers/auser.controler");
+const {  get_city_aadhar_user, user_registration, 
+         get_aadhar_count_s, get_aadhar_user_sc,
+         single_user_registration } = require("../controllers/auser.controler");
 const { uploadImage } = require("../controllers/file-upload/uploadImage");
 
 const multer = require('multer');
@@ -7,10 +9,14 @@ const upload = multer({dest: "/Uploads"});
 
 const router = express.Router();
 
+// get
 router.get("/city_aadhar_user", get_city_aadhar_user);
-router.post("/user_registration", user_registration);
 router.get("/state_wise_adhar_count_above_60", get_aadhar_count_s);
 router.get("/state_city_wise_adhar_users", get_aadhar_user_sc)
+
+// post
+router.post("/user_registration", user_registration);
+router.post("/adhar_user_registration", single_user_registration);
 router.post("/upload-profile", upload.single('profile'), uploadImage);
 
 module.exports = router;
